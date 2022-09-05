dcc49af0917396c9417850bafc8e146ee98ef787
luongminhduy <87934522+luongminhduy@users.noreply.github.com>
2022-09-05 11:49:43 +0700

    init

--- Zombie/main.py
+++ Zombie/main.py
@@ -3,12 +3,15 @@ import pygame
 pygame.init()
   
 # CREATING CANVAS
-canvas = pygame.display.set_mode((500, 500))
+canvas = pygame.display.set_mode((1000, 600))
   
 # TITLE OF CANVAS
 pygame.display.set_caption("Zombie")
 exit = False
-  
+
+#CREATE OBJECTS
+
+#GAME LOOP  
 while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
