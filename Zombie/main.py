a7c02e82e188f604f84277e176139d83b7e5be81
DESKTOP-OCKLJSV\Quynh Huong <qhuongcse19@gmail.com>
2022-09-19 00:36:53 +0700

    Add animation assets

--- Zombie/main.py
+++ Zombie/main.py
@@ -9,13 +9,16 @@ canvas = pygame.display.set_mode((940, 528))
 # TITLE OF CANVAS
 pygame.display.set_caption("Zombie")
 exit = False
+
 #Load Image
-bgImg = pygame.image.load("Zombie/assets/backGround.png")
-holeImg = pygame.image.load("Zombie/assets/hole.png")
+bgImg = pygame.image.load("assets/backGround.png")
+holeImg = pygame.image.load("assets/hole.png")
+
 #CREATE OBJECTS
 backGround = BackGround(bgImg, 0, 0, 1)
 matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 hole = Hole(120, 120, matrix, 150, holeImg, (100, 100))
+
 #GAME LOOP  
 while not exit:
     for event in pygame.event.get():
