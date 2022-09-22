3a44f4811be8f93107e45559a4645604c6495bce
DESKTOP-OCKLJSV\Quynh Huong <qhuongcse19@gmail.com>
2022-09-22 23:47:30 +0700

    Mole animation

--- Zombie/main.py
+++ Zombie/main.py
@@ -1,3 +1,5 @@
+import random
+from secrets import choice
 from tracemalloc import start
 import pygame
 from pygame import mixer
@@ -5,6 +7,7 @@ from pygame import mixer
 from objects.BackGround import BackGround
 from objects.Hole import Hole
 from objects.Hammer import Hammer
+from objects.Mole import *
 
 pygame.init()
 mixer.init()
@@ -16,13 +19,13 @@ pygame.display.set_caption("Zombie")
 exit = False
 
 #Load Image
-bgImg = pygame.image.load("./Zombie/assets/backGround.png")
-holeImg = pygame.image.load("./Zombie/assets/hole.png")
-hammerImg = pygame.image.load("./Zombie/assets/hammer_3.png")
+bgImg = pygame.image.load("assets/backGround.png")
+holeImg = pygame.image.load("assets/hole.png")
+hammerImg = pygame.image.load("assets/hammer_3.png")
 
 #Load Audio
-hit = pygame.mixer.Sound("./Zombie/assets/audio/hit.wav")
-music = pygame.mixer.Sound("./Zombie/assets/audio/music.mp3")
+hit = pygame.mixer.Sound("assets/audio/hit.wav")
+music = pygame.mixer.Sound("assets/audio/music.mp3")
 music.set_volume(0.3)
 #CREATE OBJECTS
 backGround = BackGround(bgImg, 0, 0, 1)
@@ -30,26 +33,67 @@ matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 hole = Hole(120, 120, matrix, 150, holeImg, (100, 100))
 hammer = Hammer(hammerImg, 10, hit)
 
+moles = []
+i = 0
+j = 0
+for row in matrix:
+    for mole in row:
+        moles.append(Mole(120 + 2 * i * 150 + 10, 120 + j * 150 - 42, MoleAnimation.NONE))
+        i = i + 1
+    i = 0    
+    j = j + 1 
+
+moles[random.choice(list(range(len(moles))))].animation = MoleAnimation.UP
+
 pygame.mouse.set_visible(False)
 pygame.mixer.Sound.play(music, -1)
+
 #GAME LOOP
 startTick = 0
 endTick = 0  
+
+startMoleTick = pygame.time.get_ticks()
+endMoleTick = startMoleTick
+
 while not exit:
+        
+    endMoleTick = pygame.time.get_ticks()
+    if endMoleTick - startMoleTick > 700:
+        current_mole_up = []
+    
+        for i in range(len(moles)):
+            if moles[i].animation != MoleAnimation.NONE:
+                current_mole_up.append(i)
+
+        if len(current_mole_up) <= 3:
+            new_up = random.choice(list(range(len(moles))))
+            
+            if new_up not in current_mole_up:
+                moles[new_up].animation = MoleAnimation.UP
+                
+        startMoleTick = endMoleTick
+                    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
+            
         if event.type == pygame.MOUSEBUTTONDOWN and hammer.attack == False:
             startTick = pygame.time.get_ticks()
             hammer.hit(0, 0)
             hammer.changeAnimation()
+            
     endTick = pygame.time.get_ticks()
+    
     if (startTick != 0 and endTick - startTick >= 100):
         startTick = 0
         endTick = 0
         hammer.unHit()
         hammer.changeAnimation()
+        
     backGround.display(canvas)
     hole.display(canvas)
-    hammer.display(canvas)       
+    for mole in moles:
+        mole.display(canvas)   
+    hammer.display(canvas)
+           
     pygame.display.update()
---
+++ Zombie/objects/Mole.py
@@ -0,0 +1,70 @@
+import pygame
+from enum import Enum
+
+class MoleAnimation(Enum):
+    NONE = 0
+    UP = 1
+    DOWN = 2
+    DIE = 3
+    STAND = 4
+
+class Mole:   
+ 
+    def __init__(self, pos_x, pos_y, animation = MoleAnimation.NONE) -> None:
+        super(Mole, self).__init__()
+        self.animation = animation
+        self.X = pos_x
+        self.Y = pos_y
+        self.up_frames = []
+        self.up_idx = 0
+        for i in range(1,11):
+            self.up_frames.append(pygame.transform.scale(pygame.image.load('assets/up_' + str(i) + '.png'), (90,120)))
+        
+        self.down_frames = self.up_frames[::-1]
+        self.down_idx = 0
+        
+        self.die_frames = []
+        self.die_idx = 0
+        for i in range(1,5):
+            self.up_frames.append(pygame.transform.scale(pygame.image.load('assets/die_' + str(i) + '.png'), (90,120)))
+            
+        self.stand_frame = pygame.transform.scale(pygame.image.load('assets/up_10.png'), (90,120)) 
+        self.stand_idx = 0
+        
+    def display(self, screen: pygame.Surface):
+        match(self.animation):
+            case MoleAnimation.NONE: 
+                self.up_idx = self.down_idx = self.die_idx = self.stand_idx = 0
+            
+            case MoleAnimation.UP:
+                if self.up_idx >= len(self.up_frames):
+                    self.up_idx = 0
+                    self.animation = MoleAnimation.STAND
+                    screen.blit(self.stand_frame, (self.X, self.Y))
+                else:
+                    screen.blit(self.up_frames[self.up_idx], ((self.X, self.Y)))
+                    self.up_idx += 1
+                
+            case MoleAnimation.DOWN:
+                if self.down_idx >= len(self.down_frames):
+                    self.down_idx = 0
+                    self.animation = MoleAnimation.NONE
+                else:
+                    screen.blit(self.down_frames[self.down_idx], ((self.X, self.Y)))
+                    self.down_idx += 1
+            
+            case MoleAnimation.DIE:
+                if self.die_idx >= len(self.die_frames):
+                    self.die_idx = 0
+                    self.animation = MoleAnimation.NONE
+                else:
+                    screen.blit(self.die_frames[self.die_idx], ((self.X, self.Y)))
+                    self.die_idx += 1
+                
+            case MoleAnimation.STAND:
+                if self.stand_idx >= 100:
+                    self.stand_idx = 0
+                    self.animation = MoleAnimation.DOWN
+                else:
+                    screen.blit(self.stand_frame, ((self.X, self.Y)))
+                    self.stand_idx += 1       
