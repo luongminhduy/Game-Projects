4484a1e00f0f900db026293f6399d8b32427a171
DESKTOP-QU6QUFB\Minh Duy <duy.luongminhduy@hcmut.edu.vn>
2022-12-07 14:08:35 +0700

    init FSM

--- 3DTank/Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
+++ 3DTank/Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
@@ -19,10 +19,12 @@ namespace Complete
         private float m_TurnInputValue;             // The current value of the turn input.
         private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
         private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
-
+        private bool collision ;
+        private int way = 1;
         private void Awake ()
         {
             m_Rigidbody = GetComponent<Rigidbody> ();
+            collision = false;
         }
 
 
@@ -111,8 +113,13 @@ namespace Complete
         private void FixedUpdate ()
         {
             // Adjust the rigidbodies position and orientation in FixedUpdate.
-            Move ();
-            Turn ();
+            if (m_PlayerNumber == 1) {
+                Move ();
+                Turn ();
+            }
+            else {
+                FSM ();
+            }
         }
 
 
@@ -120,22 +127,56 @@ namespace Complete
         {
             // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
             Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
-
             // Apply this movement to the rigidbody's position.
             m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
         }
-
 
         private void Turn ()
         {
             // Determine the number of degrees to be turned based on the input, speed and time between frames.
             float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+            // Make this into a rotation in the y axis.
+            Debug.Log("turn is " + turn);
+            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
 
+            // Apply this rotation to the rigidbody's rotation.
+            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+        }
+
+        private void EnemyMove (int way)
+        {
+            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+            Vector3 movement = transform.forward * way * m_Speed * Time.deltaTime;
+            // Apply this movement to the rigidbody's position.
+            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        }
+
+        private void EnemyTurn ()
+        {
+            // Determine the number of degrees to be turned based on the input, speed and time between frames.
+            float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
             // Make this into a rotation in the y axis.
             Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
 
             // Apply this rotation to the rigidbody's rotation.
             m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+        }        
+
+        private void FSM ()
+        {
+            EnemyMove(way);
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            if (other.gameObject.name != "GroundPlane") {
+                collision = true;
+                way = -way;
+            }
+        }
+        private void OnCollisionExit(Collision other) {
+            if (other.gameObject.name != "GroundPlane") {
+                collision = false;
+            }
         }
     }
 }
